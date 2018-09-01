8cc93a3abe3475c3370292c9429d35c64ad7955b
polymonster <alexandercdixon@gmail.com>
2018-09-01 16:35:43 +0100

    initial commit

---
+++ _test.cpp
@@ -0,0 +1,12 @@
+#include "vec.h"
+#include "mat.h"
+#include "quat.h"
+#include "maths.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world!\n");
+    
+    return 0;
+}
---
+++ _travis.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+c++ --std=c++11 _test.cpp -o test && ./test
